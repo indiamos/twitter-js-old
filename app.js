49c17f1bd3ca22101ef337e3dbda4d7d97885022
India Amos <india@indiamos.com>
2017-06-13 12:26:49 -0400

    Add request logging, colored console output, and support for 'npm start'

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const express = require('express');
 const app = express();
 
@@ -5,7 +6,25 @@ app.listen(3000, () => {
   console.log('server listening');
 });
 
+app.use((request, response, next) => {
+  var requestParams = Object.keys(request.params).length === 0 ? "" : request.params;
+  var requestQuery = Object.keys(request.query).length === 0 ? "" : request.query;
+  console.log(chalk.magenta(request.method), chalk.yellow(request.path), requestQuery, chalk.cyan(requestParams),response.statusCode);
+  // Why doesn't chalk work with objects, such as request.query?
+  // var queryString = request.query.toString();
+  // console.log("queryString: ",queryString);
+  next();
+})
+
+app.use("/special/", (request, response, next) => {
+  console.log("Welcome to the Special Area");
+  next();
+})
+
 app.get('/', (request, response) => {
-  response.send('Welcome to Twitter!');
+  response.send('Welcome to Twitter! This is a new get.response.');
 });
 
+app.get('/news', (request, response) => {
+  response.send('You are now in the /news path. And /news is now NEW.');
+});
