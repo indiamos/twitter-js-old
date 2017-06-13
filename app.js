869c19b9a29cb13947a3daffce369b2e819967e5
India Amos <india@indiamos.com>
2017-06-13 15:10:43 -0400

    Tidy up app.js and label things with comments

--- app.js
+++ app.js
@@ -6,20 +6,48 @@ const nunjucks = require('nunjucks');
 
 const app = express();
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// SAMPLE DATA TO FEED TO NUNJUCKS
+const people = [
+  {name: 'Full'},
+  {name: 'Stacker'},
+  {name: 'Son'}
+];
 
+const locals = {
+  title: 'An Example',
+  people: [
+    { name: 'Gandalf' },
+    { name: 'Frodo' },
+    { name: 'Hermione' }
+  ]
+};
+
+// CONFIGURE NUNJUCKS
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates, and turn off caching while we're developing
+nunjucks.render('index.html', locals, function(err, output) {
+  console.log(output);
+});
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
+// TELL THE SERVER TO LISTEN FOR REQUESTS
 app.listen(3000, () => {
   console.log('server listening');
 });
 
+// RESPOND TO REQUESTS
 app.use((request, response, next) => {
+  // LOG EACH REQUEST TO CONSOLE
   var requestParams = Object.keys(request.params).length === 0 ? "" : request.params;
   var requestQuery = Object.keys(request.query).length === 0 ? "" : request.query;
   console.log(chalk.magenta(request.method), chalk.yellow(request.path), requestQuery, chalk.cyan(requestParams), response.statusCode);
   // Why doesn't chalk work with objects, such as request.query?
   // var queryString = request.query.toString();
   // console.log("queryString: ",queryString);
-  response.render( 'index', {title: 'Hall of Fame', people: people} );
+
+  // TEST NUNJUCKS INTEGRATION
+  // response.render( 'index', {title: 'Hall of Fame', people: people} );
+  response.render( 'index', locals );
   next();
 })
 
@@ -36,19 +64,3 @@ app.use((request, response, next) => {
 //   response.render('You are now in the /news path. And /news is now NEW.');
 // });
 
-var locals = {
-  title: 'An Example',
-  people: [
-    { name: 'Gandalf' },
-    { name: 'Frodo' },
-    { name: 'Hermione' }
-  ]
-};
-
-nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates, and turn off caching while we're developing
-nunjucks.render('index.html', locals, function(err, output) {
-  console.log(output);
-});
-
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
